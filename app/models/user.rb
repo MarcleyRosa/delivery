c27e633935e948bc1f5c4139a2eb296cb8dd52be
Marcley <marcleyrosa@gmail.com>
2024-03-30 09:01:36 -0300

    feat: metodos para gerar e decodifcar token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   has_many :stores
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.from_token(token)
+    decoded = JWT.decode token, "secret", true, { algorithm: 'HS256' }
+    user_data = decoded[0].with_indifferent_access
+    User.find(user_data[:id])
+  end
+
+  def self.token_for(user)
+    payload = { id: user.id, email: user.email, role: user.role }
+    JWT.encode payload, "secret", "HS256"
+  end
+
 end
