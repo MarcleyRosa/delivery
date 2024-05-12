1cba9c44173255c42766ff009352b3f5926bf2a9
Marcley <marcleyrosa@gmail.com>
2024-05-12 03:14:42 -0300

    feat: state machine com metodo de status do pedido accept

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,23 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: "User", foreing_key: 'user_id'
   belongs_to :store, foreing_key: 'store_id'
+  has_many :order_items
+  has_many :products, through: :order_items
+  validate :buyer_role
 
-  # validate :buyer_role
+  state_machine initial: :created do
+    event :accept do
+      transition created: :accepted
+    end
+  end
+
+  def accept
+    if self.state == :created
+      update! state: :accepted
+    else
+      raise "Can't change to `accepted` from #{self.state}"
+    end
+  end
 
   private 
   def buyer_role
