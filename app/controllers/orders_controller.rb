6ae07d6348271b55ad586f004ba7d8ef880ff743
Marcley <marcleyrosa@gmail.com>
2024-05-18 19:41:12 -0300

    feat: controller de orders com index create e strong paramiters

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,28 @@
+class OrdersController < ApplicationController
+  skip_forgery_protection
+  before_action :only_buyers, :authenticate!
+
+  def index
+    @orders = Order.where(buyer: current_user)
+    render json: { orders: @orders }
+  end
+
+  def create
+    @order = Order.new(order_params) { |o| o.buyer = current_user }
+    # @order.buyer = current_user
+    # ou
+    # @order = Order.new(order_params) { |o| o.buyer = current_user }
+
+    if @order.save
+      render :create, status: :created
+    else
+      render json: { errors: @order.errors, status: :unprocessable_entity }
+    end
+  end
+
+  private 
+  def order_params
+    params.require(:order).permit([:store_id])
+  end
+
+end
