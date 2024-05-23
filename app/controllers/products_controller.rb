3c30b0a1d67ea59dafb6b32786d3161a3c5c7e67
Marcley <marcleyrosa@gmail.com>
2024-05-23 10:53:36 -0300

    feat: listando produtos por loja e adiciopnando metodo para editar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  skip_forgery_protection only: [:update]
+  before_action :authenticate_user!, only: [:listing]
+  before_action :set_product, only: [:update]
   def listing
     if !current_user.admin?
       redirect_to root_path, notice: "No permission for you!"
@@ -7,4 +9,27 @@ class ProductsController < ApplicationController
     
     @products = Product.includes(:store)
   end
+
+  def products_store
+    @products = Product.where(store_id: params[:id])
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :price)
+  end
+
 end
