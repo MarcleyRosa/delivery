b1eeef2725c6c9f8f751757f38062e0f2bfab2af
Marcley <marcleyrosa@gmail.com>
2024-05-23 10:56:20 -0300

    fix: acrescentando foreign key

--- db/migrate/20240508232926_create_orders.rb
+++ db/migrate/20240508232926_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
-      t.references :buyer, null: false
-      t.references :store, null: false
+      t.references :buyer, foreing_key: { to_table: :users }, null: false
+      t.references :store, foreing_key: true, null: false
       t.timestamps
     end
   end
