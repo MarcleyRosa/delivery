53f6b55a0cd4e18a7eb7036780df7f3ddab1cb15
Marcley <marcleyrosa@gmail.com>
2024-05-30 22:04:39 -0300

    feat: relacionamento de cart com product e user

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :store
   has_many :orders, through: :order_items
+  has_many :cart_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :role, presence: true
   enum :role, [:admin, :seller, :buyer]
+  has_one :cart, dependent: :destroy
   has_many :stores
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
