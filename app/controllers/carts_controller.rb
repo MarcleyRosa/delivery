eb86e8632c7d2a37537ecc39e0a0e4ccb3102232
Marcley <marcleyrosa@gmail.com>
2024-05-30 22:05:50 -0300

    feat: controller de cart com methodos de crud

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,37 @@
+class CartsController < ApplicationController
+  skip_forgery_protection
+  before_action :authenticate!
+  before_action :set_cart, only: [:add_item, :remove_item]
+
+  def show
+    user = Cart.find_by(user_id: current_user.id)
+    render json: user, include: { cart_items: { include: :product } }
+  end
+
+  def add_item
+    product = Product.find(params[:product_id])
+    quantity = params[:quantity]
+    cart_item = @cart.cart_items.find_by(product: product)
+
+    if cart_item
+      cart_item.quantity += quantity
+    else
+      cart_item = @cart.cart_items.build(product: product, quantity: quantity, price: product.price)
+    end
+
+    cart_item.save
+    render json: cart_item
+  end
+
+  def remove_item
+    cart_item = @cart.cart_items.find_by(product_id: params[:product_id])
+    cart_item.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_cart
+    @cart = current_user.cart || current_user.create_cart
+  end
+end
