7d5159c08d5cba517f3ff01c845cbe8cd7a33856
Marcley <marcleyrosa@gmail.com>
2024-06-01 00:46:20 -0300

    feat: method para deletar carrinho depois de finalizar a compra

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,15 @@
 class CartsController < ApplicationController
   skip_forgery_protection
   before_action :authenticate!
-  before_action :set_cart, only: [:add_item, :remove_item]
+  before_action :set_cart, only: [:show, :add_item, :remove_item, :destroy]
 
   def show
-    user = Cart.find_by(user_id: current_user.id)
-    render json: user, include: { cart_items: { include: :product } }
+    # user = Cart.find_by(user_id: current_user.id)
+    # render json: user, include: { cart_items: { include: :product } }
+    # @cart = current_user.cart
+    @cart_items = @cart.cart_items.includes(:product)
+    
+    render json: @cart_items, include: :product
   end
 
   def add_item
@@ -14,7 +18,7 @@ class CartsController < ApplicationController
     cart_item = @cart.cart_items.find_by(product: product)
 
     if cart_item
-      cart_item.quantity += quantity
+      cart_item.quantity = quantity
     else
       cart_item = @cart.cart_items.build(product: product, quantity: quantity, price: product.price)
     end
@@ -26,6 +30,11 @@ class CartsController < ApplicationController
   def remove_item
     cart_item = @cart.cart_items.find_by(product_id: params[:product_id])
     cart_item.destroy
+    head :no_content
+  end
+
+  def destroy
+    @cart.destroy!
     head :no_content
   end
 
