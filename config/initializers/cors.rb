799560add7d450212bb7cbcf56c088cc89af77ba
Marcley <marcleyrosa@gmail.com>
2024-06-01 00:45:16 -0300

    feat: adicionando metodo delete para rota de delete de produtos no carrinho

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
+    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    resource "/cart/remove_item/:id", headers: :any, methods: [:delete]
   end
 end
