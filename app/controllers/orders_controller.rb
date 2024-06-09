c361e2ac54f172a3975b41bf68d31d643c2fbc40
Marcley <marcleyrosa@gmail.com>
2024-06-09 14:24:47 -0300

    feat: class para criar pagamento em pedidos

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,14 +9,21 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params) { |o| o.buyer = current_user }
-    # @order.buyer = current_user
-    # ou
-    # @order = Order.new(order_params) { |o| o.buyer = current_user }
 
     if @order.save
+      
       order_items_params.each do |item_params|
         @order.order_items.create(item_params)
       end
+      PaymentJob.perform_later(
+        payment: {
+          order: @order,
+          value: payment_params[:value],
+          valid: payment_params[:valid],
+          number: payment_params[:number],
+          cvv: payment_params[:cvv]
+        }
+      )
       render :create, status: :created
     else
       render json: { errors: @order.errors, status: :unprocessable_entity }
@@ -24,6 +31,10 @@ class OrdersController < ApplicationController
   end
 
   private 
+  def payment_params
+    params.require(:payment).permit(:value, :number, :valid, :cvv)
+  end
+
   def order_params
     params.require(:order).permit([:store_id])
   end
