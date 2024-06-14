287c1b388b1ba798fd1a5b517608c953851496e6
Marcley <marcleyrosa@gmail.com>
2024-06-14 17:42:17 -0300

    feat: adicionando coluna ctive para indicar stores que abertas ou fechadas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_173053) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_142738) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_06_173053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "active", default: false, null: false
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
