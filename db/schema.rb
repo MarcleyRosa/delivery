cbe8ed61f4759bc2a4e4969aab02976dcb0f9096
Marcley <marcleyrosa@gmail.com>
2024-06-15 01:52:41 -0300

    feat: adicionando stock para produto e no schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_142738) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_15_021341) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_142738) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock", default: 0, null: false
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
