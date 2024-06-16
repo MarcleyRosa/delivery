238f0c6a4a9598d9355e339b4f7757cd78c98e3d
Marcley <marcleyrosa@gmail.com>
2024-06-16 16:37:59 -0300

    feat: mudando cart-item para adicionar mais de um produto

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,19 +13,30 @@ class CartsController < ApplicationController
   end
 
   def add_item
-    product = Product.find(params[:product_id])
-    quantity = params[:quantity]
-    cart_item = @cart.cart_items.find_by(product: product)
+    products_params = product_params
+    updated_cart_items = []
 
-    if cart_item
-      cart_item.quantity = quantity
-    else
-      cart_item = @cart.cart_items.build(product: product, quantity: quantity, price: product.price)
+    products_params.each do |product_param|
+      product = Product.find(product_param[:product_id])
+      quantity = product_param[:quantity]
+      cart_item = @cart.cart_items.find_by(product: product)
+
+      if cart_item
+        cart_item.quantity = quantity
+      else
+        cart_item = @cart.cart_items.build(product: product, quantity: quantity, price: product.price)
+      end
+
+      if cart_item.save
+        updated_cart_items << cart_item
+      else
+        render json: { error: "Failed to save cart item" }, status: :unprocessable_entity and return
+      end
     end
 
-    cart_item.save
-    render json: cart_item
+    render json: updated_cart_items
   end
+  
 
   def remove_item
     cart_item = @cart.cart_items.find_by(product_id: params[:product_id])
@@ -39,6 +50,12 @@ class CartsController < ApplicationController
   end
 
   private
+
+  def product_params
+    params.require(:products).map do |product|
+      product.permit(:product_id, :quantity)
+    end
+  end
 
   def set_cart
     @cart = current_user.cart || current_user.create_cart
