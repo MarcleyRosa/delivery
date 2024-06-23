e5141ede7ce8f404f2305f3c2bae9a2cea708393
Marcley <marcleyrosa@gmail.com>
2024-06-23 03:44:50 -0300

    feat: migration e schema de product para adcionar discar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_051839) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_23_054131) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -106,6 +106,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_23_051839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock", default: 0, null: false
+    t.datetime "discarded_at"
+    t.index ["discarded_at"], name: "index_products_on_discarded_at"
     t.index ["store_id"], name: "index_products_on_store_id"
   end
 
