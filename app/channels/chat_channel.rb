b7458bc801df8b4a224f68bde4bab3a3443b995f
Marcley <marcleyrosa@gmail.com>
2024-06-27 17:09:56 -0300

    feat: mensagem em tempo real entra as aplicações ok

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,11 +1,7 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    sender_id = params[:sender_id]
-    receiver_id = params[:receiver_id]
-
-    # Stream for both directions
-    stream_from "chat_channel_#{sender_id}_#{receiver_id}"
-    stream_from "chat_channel_#{receiver_id}_#{sender_id}"
+    stream_from "chat_#{params[:sender_id]}_#{params[:receiver_id]}"
+    stream_from "chat_#{params[:receiver_id]}_#{params[:sender_id]}"
   end
 
   def unsubscribed
@@ -13,21 +9,8 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    sender_id = params[:sender_id]
-    receiver_id = params[:receiver_id]
-    message = Message.create!(sender_id: sender_id, receiver_id: receiver_id, content: data['message'])
-
-    # Broadcast to both sender and receiver streams
-    ActionCable.server.broadcast("chat_channel_#{sender_id}_#{receiver_id}", { message: render_message(message) })
-    ActionCable.server.broadcast("chat_channel_#{receiver_id}_#{sender_id}", { message: render_message(message) })
-  end
-
-  private
-
-  def render_message(message)
-    ApplicationController.render(
-      partial: 'messages/message',
-      locals: { message: message }
-    )
+    message = data['message']
+    ActionCable.server.broadcast("chat_#{params[:sender_id]}_#{params[:receiver_id]}", { message: message })
+    ActionCable.server.broadcast("chat_#{params[:receiver_id]}_#{params[:sender_id]}", { message: message })
   end
 end
